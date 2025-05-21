0bf83dc2dcee90d8c927200d73179292c39dae8b
Hina Liaqat <m.asfandnaveed@gmail.com>
2025-05-21 16:30:04 +0500

    changes

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,20 +2,54 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-const db = require('./db');
-
-//Middelware
+const database = require('./db');
+//Middleware
 app.use(express.json());
 
+
 app.get('/users',(req,res)=>{
-    db.query('SELECT * FROM users',(error,result)=>{
+    database.query('SELECT * FROM users', (error , result)=>{
+        
         if(error){
-            console.log('Something Went Wrong',error);
+            console.log('ERROR',error);
         }
-        // res.status(200).send('Test route working');
+
         res.json(result);
     });
 });
+
+app.post('/users/login',(req,res)=>{
+
+    const {email , pass} = req.body;
+
+    if(email && pass){
+
+        const query = "SELECT * FROM users WHERE email = ? AND password=?";
+
+        database.query(query,[email,pass], (error, result)=>{
+            
+            if(error){
+                console.log("ERROR",error);
+            }
+
+            if(result.length > 0 ){
+                res.json(result);
+            }else{
+                res.json({
+                    message:"Inavild Credentials !!"
+                });
+            }
+
+        });
+
+    }else{
+
+        res.json({
+            message:"Email and Password are required !!"
+        });
+    }
+});
+
 
 
 app.listen(port,()=>{
--- nodejs/db.js
+++ nodejs/db.js
@@ -1,12 +1,11 @@
-const mysql = require('mysql2');
+const mysql =require('mysql2')
 
-
-const pool = mysql.createPool({
+const pool= mysql.createPool({
     host:"localhost",
     user:"root",
     password:"",
     database:"corvit_eve_10_03_2025",
-    connectionLimit:20
+    connectionLimit:25
 });
 
 module.exports = pool;
